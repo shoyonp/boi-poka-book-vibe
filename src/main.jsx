e5320d81461092749667a38f9990243d5250e744
shoyonp <shoyonkumar143@gmail.com>
2024-11-01 06:31:52 +0600

    added toast

--- src/main.jsx
+++ src/main.jsx
@@ -11,6 +11,8 @@ import Home from './components/Home/Home';
 import Dashboard from './components/Dashboard/Dashboard';
 import BookDetail from './components/BookDetail/BookDetail';
 import ListedBooks from './components/ListedBooks/ListedBooks';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const router = createBrowserRouter([
   {
@@ -44,5 +46,6 @@ const router = createBrowserRouter([
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <RouterProvider router={router} />
+    <ToastContainer></ToastContainer>
   </StrictMode>,
 )
--- src/utility/addToDB.js
+++ src/utility/addToDB.js
@@ -1,3 +1,5 @@
+import { toast } from "react-toastify";
+
 const getStoredReadList = () => {
   // read-list
   const storedListStr = localStorage.getItem("read-list");
@@ -18,6 +20,8 @@ const addToStoredReadList = (id) => {
     storedList.push(id);
     const storedListStr = JSON.stringify(storedList);
     localStorage.setItem("read-list", storedListStr);
+    // ideally tigger toast from the component
+    toast('this book is added to your read list')
   }
 };
 
